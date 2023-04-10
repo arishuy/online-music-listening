453275baded4aab50d928a5b24742d06edd326ea
arishuy <huybui19898@gmail.com>
2023-04-10 14:06:42 +0700

    add music page

--- music/urls.py
+++ music/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('music/', views.music, name='music'),
+    path('music/<int:song_id>/', views.detail, name='detail'),
     path('', views.homepage, name='hompage'),
     path('recent/', views.recent, name='recent'),
     path('playlists/', views.playlists, name='playlists'),
--- music/views.py
+++ music/views.py
@@ -12,3 +12,5 @@ def recent(request):
     return HttpResponse("Hello, world. You're at the recent index.")
 def playlists(request):
     return HttpResponse("Hello, world. You're at the playlists index.")
+def detail(request, song_id):
+    return render(request, 'detailsong.html', {'song_id': song_id})
