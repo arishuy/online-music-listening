6408cbf867e7ff4db0080b06cf7f50bdf44b2713
arishuy <huybui19898@gmail.com>
2023-04-10 14:36:47 +0700

    add playist page + fix wrong name file

--- music/views.py
+++ music/views.py
@@ -10,7 +10,9 @@ def homepage(request):
     return render(request, 'homepage.html')
 def recent(request):
     return render(request, 'recentlisten.html')
+
 def playlists(request):
-    return HttpResponse("Hello, world. You're at the playlists index.")
+    return render(request, 'playlists.html')
+
 def detail(request, song_id):
     return render(request, 'detailsong.html', {'song_id': song_id})
