88a4c84075549d1f0f9032f5258c10cd3764fdf5
HungNG2k2 <hung200220@gmail.com>
2023-04-17 16:45:29 +0700

    add audio element

--- cuoiky_python/urls.py
+++ cuoiky_python/urls.py
@@ -15,9 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.contrib.staticfiles.urls import static
+from django.conf import settings
 
 urlpatterns = [
     path('', include('music.urls')),
     path('', include('users.urls')),
     path('admin/', admin.site.urls),
 ]
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
