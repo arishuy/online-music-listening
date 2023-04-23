bf606f8832a04fcbc7013f2b466247c90f87bda0
HungNG2k2 <hung200220@gmail.com>
2023-04-23 18:23:18 +0700

    add next song play

--- music/models.py
+++ music/models.py
@@ -1,23 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+
 class Artist(models.Model):
     name = models.CharField(max_length=50)
     about = models.CharField(max_length=2000)
 
-
     def __str__(self) -> str:
         return self.name
-    
 
 
 class Genre(models.Model):
     name = models.CharField(max_length=30)
 
-
     def __str__(self) -> str:
         return self.name
-    
 
 
 class Album(models.Model):
@@ -25,36 +23,33 @@ class Album(models.Model):
     artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
     release_day = models.DateField()
 
-
     def __str__(self) -> str:
         return self.name
-
 
 
 class Song(models.Model):
     name = models.CharField(max_length=100)
     stream_count = models.IntegerField(default=0)
-    link = models.CharField(max_length=100)
+    image = models.CharField(max_length=100, default="")
+    audio_file = models.FileField(blank=True, null=True, upload_to="audio/")
+    audio_link = models.CharField(max_length=100, blank=True, null=True)
     release_day = models.DateField()
     duration = models.IntegerField()
-    album = models.ForeignKey(Album, null=True, blank=True, on_delete=models.SET_NULL)
+    album = models.ForeignKey(
+        Album, null=True, blank=True, on_delete=models.SET_NULL)
     artists = models.ManyToManyField(Artist)
     genres = models.ManyToManyField(Genre)
 
-
     def __str__(self) -> str:
         return self.name
-    
 
 
 class User(models.Model):
     username = models.CharField(max_length=50, unique=True)
     password = models.CharField(max_length=30)
 
-
     def __str__(self) -> str:
         return self.username
-    
 
 
 class Playlist(models.Model):
@@ -62,7 +57,6 @@ class Playlist(models.Model):
     song_list = models.ManyToManyField(Song)
     create_date = models.DateField()
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
-
 
     def __str__(self) -> str:
         return self.name
--- music/views.py
+++ music/views.py
@@ -1,18 +1,35 @@
+import json
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Song
 
 # Create your views here.
+
 
 def music(request):
     return HttpResponse("Hello, world. You're at the music index.")
 
+
 def homepage(request):
-    return render(request, 'homepage.html')
+    songs = Song.objects.all()
+    print(songs)
+    songJson = list(map(lambda song: {
+        "name": song.name,
+        "artists": list(map(lambda artist: {
+            "name": artist.name
+        }, song.artists.all())),
+        "audio": song.audio_file.url if song.audio_file else song.audio_link,
+    }, songs))
+    return render(request, 'homepage.html', {'songs': json.dumps(songJson)})
+
+
 def recent(request):
     return render(request, 'recentlisten.html')
 
+
 def playlists(request):
     return render(request, 'playlists.html')
+
 
 def detail(request, song_id):
     return render(request, 'detailsong.html', {'song_id': song_id})
